983c89492f538dae94f0b9b7c1bcc3c3f997e16d
Andreas Schneider <aksdb@gmx.de>
2025-12-09 19:59:57 +0100

    💄 Prepare main window

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace perinma.Views;
 
@@ -7,5 +10,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void MnuExit_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
     }
 }
