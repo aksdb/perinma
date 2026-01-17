b125bf931534ca8b4fc3350a5dfe06023f42ce80
Andreas Schneider <aksdb@gmx.de>
2026-01-17 12:49:27 +0100

    💄 Make accounts collapsible

--- src/Views/CalendarList/AccountGroupViewModel.cs
+++ src/Views/CalendarList/AccountGroupViewModel.cs
@@ -12,5 +12,8 @@ public partial class AccountGroupViewModel : ObservableObject
     [ObservableProperty]
     private string _accountName = string.Empty;
 
+    [ObservableProperty]
+    private bool _isExpanded = true;
+
     public ObservableCollection<CalendarViewModel> Calendars { get; } = new();
 }
