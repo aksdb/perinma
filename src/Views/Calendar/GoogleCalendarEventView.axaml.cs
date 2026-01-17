9d6a02fcd6b6adb98723e8c50e665c7b51db55bd
Andreas Schneider <aksdb@gmx.de>
2026-01-17 17:36:32 +0100

    ✨ Make links clickable

--- src/Views/Calendar/GoogleCalendarEventView.axaml.cs
+++ src/Views/Calendar/GoogleCalendarEventView.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using perinma.Utils;
+using TheArtOfDev.HtmlRenderer.Avalonia;
+using TheArtOfDev.HtmlRenderer.Core.Entities;
 
 namespace perinma.Views.Calendar;
 
@@ -7,5 +11,24 @@ public partial class GoogleCalendarEventView : UserControl
     public GoogleCalendarEventView()
     {
         InitializeComponent();
+        DescriptionHtmlPanel.LinkClicked += OnHtmlLinkClicked;
+    }
+
+    private void OnGoogleMeetLinkPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (DataContext is GoogleCalendarEventViewModel viewModel && !string.IsNullOrEmpty(viewModel.GoogleMeetLink))
+        {
+            PlatformUtil.OpenBrowser(viewModel.GoogleMeetLink);
+            e.Handled = true;
+        }
+    }
+
+    private void OnHtmlLinkClicked(object? sender, HtmlRendererRoutedEventArgs<HtmlLinkClickedEventArgs> e)
+    {
+        if (!string.IsNullOrEmpty(e.Event?.Link))
+        {
+            PlatformUtil.OpenBrowser(e.Event.Link);
+            e.Event.Handled = true;
+        }
     }
 }
