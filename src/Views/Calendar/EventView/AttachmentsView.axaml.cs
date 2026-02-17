b2f44de4055f0a10ae521b54c38f0f7945bc721e
Andreas Schneider <aksdb@gmx.de>
2026-02-17 12:59:20 +0100

    🐛 Fix links not being clickable

--- src/Views/Calendar/EventView/AttachmentsView.axaml.cs
+++ src/Views/Calendar/EventView/AttachmentsView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using perinma.Utils;
 
 namespace perinma.Views.Calendar.EventView;
 
@@ -7,5 +9,17 @@ public partial class AttachmentsView : UserControl
     public AttachmentsView()
     {
         InitializeComponent();
+    }
+
+    private void OnAttachmentPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is not TextBlock textBlock || e.GetCurrentPoint(textBlock).Properties.IsLeftButtonPressed != true)
+            return;
+
+        if (textBlock.DataContext is AttachmentItemViewModel viewModel)
+        {
+            PlatformUtil.OpenBrowser(viewModel.Url);
+            e.Handled = true;
+        }
     }
 }
--- src/Views/Calendar/EventView/ConferenceView.axaml.cs
+++ src/Views/Calendar/EventView/ConferenceView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using perinma.Utils;
 
 namespace perinma.Views.Calendar.EventView;
 
@@ -7,5 +9,17 @@ public partial class ConferenceView : UserControl
     public ConferenceView()
     {
         InitializeComponent();
+    }
+
+    private void OnUriPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is not TextBlock textBlock || e.GetCurrentPoint(textBlock).Properties.IsLeftButtonPressed != true)
+            return;
+
+        if (textBlock.DataContext is ConferenceEntryPointViewModel viewModel)
+        {
+            PlatformUtil.OpenBrowser(viewModel.Uri);
+            e.Handled = true;
+        }
     }
 }
