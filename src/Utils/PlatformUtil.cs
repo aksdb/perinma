12f1aee8ee828b6712620725fe1bd7b7306fe796
Andreas Schneider <aksdb@gmx.de>
2026-02-17 18:43:29 +0100

    ✨ Add ability to copy and open attachment URLs

--- src/Utils/PlatformUtil.cs
+++ src/Utils/PlatformUtil.cs
@@ -1,5 +1,10 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input.Platform;
+using Avalonia.VisualTree;
 
 namespace perinma.Utils;
 
@@ -15,12 +20,14 @@ public static class PlatformUtil
             {
                 Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
                 return true;
-            } 
+            }
+
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
                 Process.Start("xdg-open", url);
                 return true;
             }
+
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 Process.Start("open", url);
@@ -31,6 +38,29 @@ public static class PlatformUtil
         {
             // Suppress errors and just consider this as "didn't succeed".
         }
+
         return false;
+    }
+
+    public static IClipboard Clipboard()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime
+            {
+                MainWindow: { } window
+            })
+        {
+            return window.Clipboard!;
+        }
+
+        if (Application.Current?.ApplicationLifetime is ISingleViewApplicationLifetime { MainView: { } mainView })
+        {
+            var visualRoot = mainView.GetVisualRoot();
+            if (visualRoot is TopLevel topLevel)
+            {
+                return topLevel.Clipboard!;
+            }
+        }
+
+        return null!;
     }
 }
--- src/Views/Calendar/EventView/AttachmentsViewModel.cs
+++ src/Views/Calendar/EventView/AttachmentsViewModel.cs
@@ -2,13 +2,15 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using perinma.Models;
+using perinma.Utils;
 
 namespace perinma.Views.Calendar.EventView;
 
 public partial class AttachmentsViewModel(List<CalendarEventAttachment> attachments) : ViewModelBase
 {
-    public ObservableCollection<AttachmentItemViewModel> Attachments { get; } = 
+    public ObservableCollection<AttachmentItemViewModel> Attachments { get; } =
         [..attachments.Select(a => new AttachmentItemViewModel(a))];
 }
 
@@ -19,4 +21,16 @@ public partial class AttachmentItemViewModel(CalendarEventAttachment attachment)
 
     [ObservableProperty]
     private string _url = attachment.Url;
+
+    [RelayCommand]
+    private void CopyUrl()
+    {
+        PlatformUtil.Clipboard().SetTextAsync(Url);
+    }
+
+    [RelayCommand]
+    private void OpenUrl()
+    {
+        PlatformUtil.OpenBrowser(Url);
+    }
 }
